822e023a59fa13cd6a90827c256e3d96f70eaded
Tawishi <tawishisharma1@gmail.com>
2021-01-16 18:03:55 +0530

    Added constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const CHANNEL_NAME = '#MLH';
+export const OAUTH_TOKEN = 'oauth:xg0o5i2vkbei1awdxrui72u99fso42';
+export const BOT_USERNAME = 'tawishi9';
+export const BLOCKED_WORDS = [
+  'cats',
+  'dogs',
+]
